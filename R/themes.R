778334be8bc2c2119604fe579f0d68ec0c280e6f
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-05-11 22:43:47 -0400

    Use inline R code in documentation

--- R/themes.R
+++ R/themes.R
@@ -5,20 +5,7 @@
 #' @section cleanrmd theme list:
 #'
 #' \pkg{cleanrmd} includes the following no-class CSS themes:
-#' - [awsm.css](https://igoradamenko.github.io/awsm.css/)
-#' - [bahunya](https://kimeiga.github.io/bahunya/)
-#' - [kacit](https://kimeiga.github.io/kacit/)
-#' - [marx](https://mblode.github.io/marx/)
-#' - [minicss](https://minicss.org/)
-#' - [no-class](https://davidpaulsson.github.io/no-class/)
-#' - [sakura](https://oxal.org/projects/sakura/)
-#' - [sakura-vader](https://oxal.org/projects/sakura/)
-#' - [stylize](https://gadenbuie.github.io/stylize.css/) (forked from [vasanthv/stylize.css](https://github.com/vasanthv/stylize.css)
-#' - [tacit](https://yegor256.github.io/tacit/)
-#' - [vanilla](https://vanillacss.com/)
-#' - [water](https://kognise.github.io/water.css/)
-#' - [water-dark](https://kognise.github.io/water.css/)
-#' - [writ](https://writ.cmcenroe.me/)
+#' `r cleanrmd_theme_list_roxygen()`
 #'
 #' @return A character string of available theme names.
 #'
@@ -47,14 +34,12 @@ cleanrmd_theme_file <- function(file) {
 }
 
 cleanrmd_theme_list_roxygen <- function() {
-  cat(
   paste0(
-      "#' - [",
+    "- [",
     cleanrmd_theme_list$name,
     "](",
     cleanrmd_theme_list$url,
-      ")"
-    ),
-    sep = "\n"
+    ")",
+    collapse = "\n"
   )
 }
